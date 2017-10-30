76e9040d8ba57c52a9054983218898758f505a2b
ramborell23 <jerelldavis@ac.c4q.nyc>
2017-10-30 16:24:52 -0400

    element exercise

---
+++ element.js
@@ -0,0 +1,10 @@
+var input = window.prompt('Write an entire HTML element')
+var x = document.getElementById("new");
+console.log(x)
+
+x.innerHTML= input
+
+
+
+// innerText shows input exactly as is 
+// innerHTML takes html tags
